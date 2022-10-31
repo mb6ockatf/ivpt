f6be32ec278c96a359793a0d5fca6466d6eed906
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-10-31 18:53:27 +0300

    refactor
    add typehints and change single-quotes to double-quotes

--- src/chemistry.py
+++ src/chemistry.py
@@ -6,14 +6,15 @@ from re import search
 class ChemistryLogicChecks(object):
     @staticmethod
     def make_abb(abb: str) -> str:
-        assert not bool(search(r'\d', abb)), "no digits in element's abbreviation"
+        assert not bool(search(r"\d", abb)), "no digits in element's abbreviation"
         assert len(abb) < 3, "abbreviation is shorter, than 3 characters"
+        assert len(abb) > 0, "abbreviation has a positive length"
         abb = abb.lower()
         abb = abb.capitalize()
         return abb
 
     @staticmethod
-    def check_number(number: int):
+    def check_number(number: int) -> int:
         assert number > 0, "element's number is a positive value"
         return number
 
@@ -23,15 +24,20 @@ class ChemistryLogicChecks(object):
             assert metal, "amphoteric element always has metal properties"
 
     @staticmethod
-    def check_period(period: int):
+    def check_period(period: int) -> int:
         assert 0 < period, "period value is bigger than 0"
         assert period < 19, "period value is less than 19"
+        return period
 
     @staticmethod
-    def check_energy_levels(energy_level_value: int):
+    def check_energy_levels(energy_level_value: int) -> int:
         assert 0 < energy_level_value, "energy level is bigger than 0"
         assert energy_level_value < 8, "energy level is smaller than 8"
+        return energy_level_value
 
-
-if __name__ == "__main__":
-    ChemistryLogicChecks.make_abb("AB")
+    @staticmethod
+    def make_element_name(element_name: str) -> str:
+        assert len(element_name) > 0
+        element_name = element_name.lower()
+        element_name = element_name.capitalize()
+        return element_name
