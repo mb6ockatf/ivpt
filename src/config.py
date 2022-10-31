10a384e66a805654d0b94c5466008940e53442dc
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-10-31 18:54:16 +0300

    fix touch_config

--- src/config.py
+++ src/config.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 
-from os import path, mkdir
+from os import path, mkdir, sep
 from tomli import load, TOMLDecodeError
-from errors_handler import ErrorHandler
+from src.errors_handler import ErrorHandler
 
 
 class Configuration(object):
 	HOME = path.expanduser("~")
-	CONFIG_DIR = HOME + "./IVPT"
-	CONFIG_FILE = HOME + CONFIG_DIR + "/config.toml"
+	CONFIG_PATH = HOME + "/.IVPT/config.toml"
 	CONFIG = None
 
 	@classmethod
 	def touch_config(cls):
-		try:
-			if not path.isdir(cls.CONFIG_DIR):
-				mkdir(cls.CONFIG_DIR)
-			if not path.isfile(cls.CONFIG_FILE):
-				open(cls.CONFIG_FILE, 'w').close()
-		except Exception as error:
-			ErrorHandler.show_exception(error)
+		config_path = cls.CONFIG_PATH
+		path_items_list = list(config_path.split(sep))[1:]
+		for index in range(len(path_items_list)):
+			cur_dir = sep + sep.join(path_items_list[0: index])
+			if not path.isdir(cur_dir):
+				mkdir(cur_dir)
+		if not path.isfile(config_path):
+			default_config = Configuration.get_default_config("default_config.toml")
+			with open(config_path, "w") as input_file:
+				input_file.write(default_config)
 
 	@classmethod
 	def read_config(cls):
+		config_path = cls.CONFIG_PATH
 		try:
-			file = open(cls.CONFIG_FILE, "rb")
-			toml_dict = load(file)
-			cls.CONFIG = toml_dict
-			file.close()
+			with open(config_path, "rb") as file:
+				toml_dict = load(file)
+				cls.CONFIG = toml_dict
 		except TOMLDecodeError as error:
 			ErrorHandler.show_exception(error)
 		except ValueError as error:
@@ -36,6 +38,8 @@ class Configuration(object):
 			print("-" * 100)
 			ErrorHandler.show_exception(error)
 
-
-"""if __name__ == "__main__":
-	stuff = ConfigurationStorage()"""
+	@staticmethod
+	def get_default_config(config_path: str):
+		with open(config_path, "r") as output_file:
+			data = output_file.read()
+		return data
