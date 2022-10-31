2d7e1a685227ba15bb8d194dc0ab1d79c2ca1bf7
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-10-31 18:51:39 +0300

    feature: select data from DB

---
+++ src/database/select.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from base import DatabaseConnection
+
+
+class DatabaseSelect(DatabaseConnection):
+    def select_basic_info(self):
+        query = """
+        SELECT number, abbreviation
+        FROM basic_info
+        ORDER BY 1;
+        """
+        self.execute_query(query, None)
+
+    def select_basic_params(self, element_number: int):
+        query = """
+        SELECT name, group, period
+        FROM basic_param
+        WHERE number = ?;
+        """
+        self.execute_query(query, (element_number,))
+
+    def select_congregation(self, element_number: int):
+        query = """
+        SELECT energy_levels, period, metal, amphoteric, gas, semiconductor
+        FROM congregation
+        WHERE number = ?;
+        """
+        self.execute_query(query, (element_number,))
+
+    @staticmethod
+    def select_links(self, element_number: int):
+        query = """
+        SELECT wiki_link FROM links
+        WHERE number = ?;
+        """
+        self.execute_query(query, (element_number,))
