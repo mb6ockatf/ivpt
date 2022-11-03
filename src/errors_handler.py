0f540926a261068e52fcdad78c0fbc6a79038d9a
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-04 01:11:53 +0300

    refactor
    turn the only class method into a standalone funtion
    imports from other files are already corrected

--- src/errors_handler.py
+++ src/errors_handler.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3ErrorHandler
+"""Pretty exception stuff"""
 from sys import exc_info
 from traceback import format_exception
 from logging import error
 
 
-class ErrorHandler(object):
-	@staticmethod
-	def show_exception(exception_object):
-		exc_type, exc_value, exc_tb = exc_info()
-		pretty_exception = format_exception(exc_type, exc_value, exc_tb)
-		error(pretty_exception)
-		error_string = " ".join(list(map(str, exception_object.args)))
-		print("Error:", error_string)
-		print("Exception class:", exception_object.__class__)
-		print("Traceback:", "\n", "\n".join(pretty_exception))
+def show_exception(exception_object):
+    """Print pretty exception"""
+    exc_type, exc_value, exc_tb = exc_info()
+    pretty_exception = format_exception(exc_type, exc_value, exc_tb)
+    error(pretty_exception)
+    error_string = " ".join(list(map(str, exception_object.args)))
+    print("Error:", error_string)
+    print("Exception class:", exception_object.__class__)
+    print("Traceback:", "\n", "\n".join(pretty_exception))
