8c0b10823f3c6ae59f6277c9a5f6f55c09a31f1f
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-04 01:07:14 +0300

    fix database tests

--- tests/test_database_insert.py
+++ tests/test_database_insert.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+"""TestDatabaseConnection testcase"""
 import unittest
 from src.database.create_table import DatabaseCreateTable
 from src.database.insert import DatabaseInsert
@@ -7,71 +7,100 @@ from src.config import Configuration
 
 
 class TestDatabaseConnection(unittest.TestCase):
+    """Test database insert methods"""
     def setUp(self):
+        """Create tables & open database connection"""
         self.config = Configuration()
         self.config.touch_config()
         self.config.read_config()
-        with DatabaseCreateTable(self.config) as self.table_creator:
-            self.table_creator.create_all_tables()
+        self.table_creator = DatabaseCreateTable(self.config)
+        self.table_creator.create_all_tables()
+        self.table_creator.close()
         self.database = DatabaseInsert(self.config)
-        self.database.__enter__()
 
     def test_execute_any_query(self):
+        """Test if at least any query can be executed"""
         self.database.execute_query("SELECT * from sqlite_master;", None)
 
     def test_basic_info(self):
+        """Test if right values can be inserted into basic_info table"""
         self.database.insert_basic_info(12, "Na")
 
     def test_basic_info_bad_params(self):
-        self.assertRaises(AssertionError, self.database.insert_basic_info, (12,  "CHLORINE"))
-        self.assertRaises(AssertionError, self.database.insert_basic_info, (0,   "Na"))
-        self.assertRaises(AssertionError, self.database.insert_basic_info, (-20, "Na"))
-        self.assertRaises(AssertionError, self.database.insert_basic_info, (12,  ""))
+        """Test if AssertionError raises on bad queries to basic_info table"""
+        params = {"number": 12, "abb": "CHLORINE"}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_info,
+                          **params)
+        params = {"number": 0, "abb": "Na"}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_info,
+                          **params)
+        params = {"number": -20, "abb": "Na"}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_info,
+                          **params)
+        params = {"number": 12, "abb": ""}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_info,
+                          **params)
 
     def test_basic_params(self):
+        """Test if right values can be inserted into basic_params table"""
         params = {"number": 12,
                   "name":   "Sulfur",
-                  "group":  "chalcogen",
+                  "grp":  "chalcogen",
                   "weight": 32.06,
-                  "period": 16}  # TODO
+                  "period": 16}
         self.database.insert_basic_params(**params)
 
     def test_basic_params_bad_params(self):
-        bad_params = {"number": -10,
-                      "name":   "Phosphorus",
-                      "group":  "pnictogen",
-                      "weight": 30.97,
-                      "period": 15}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number": -10,
-                      "name":   "Phosphorus",
-                      "group":  "somestuff",
-                      "weight": 30.97,
-                      "period": 15}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number": -10,
-                      "name":   "Phosphorus",
-                      "group":  "pnictogen",
-                      "weight": 30.97,
-                      "period": -3}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number": -10,
-                      "name":   "Phosphorus",
-                      "group":  "pnictogen",
-                      "weight": 30.97,
-                      "period": 22}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number": -10,
-                      "name":   "",
-                      "group":  "pnictogen",
-                      "weight": 30.97,
-                      "period": 15}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
+        """Test if AssertionError raises on bad queries to basic_params table"""
+        params = {"number": -10,
+                  "name":   "Phosphorus",
+                  "grp":    "pnictogen",
+                  "weight": 30.97,
+                  "period": 15}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_params,
+                          **params)
+        params = {"number": -10,
+                  "name":   "Phosphorus",
+                  "grp":    "somestuff",
+                  "weight": 30.97,
+                  "period": 15}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_params,
+                          **params)
+        params = {"number": -10,
+                  "name":   "Phosphorus",
+                  "grp":    "pnictogen",
+                  "weight": 30.97,
+                  "period": -3}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_params,
+                          **params)
+        params = {"number": -10,
+                  "name":   "Phosphorus",
+                  "grp":    "pnictogen",
+                  "weight": 30.97,
+                  "period": 22}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_params,
+                          **params)
+        params = {"number": -10,
+                  "name":   "",
+                  "grp":    "pnictogen",
+                  "weight": 30.97,
+                  "period": 15}
+        self.assertRaises(AssertionError,
+                          self.database.insert_basic_params,
+                          **params)
 
     def test_congregation(self):
+        """Test if right values can be inserted into congregation table"""
         params = {"number":        12,
                   "energy_levels": 5,
-                  "period":        11,
                   "metal":         True,
                   "amphoteric":    True,
                   "gas":           False,
@@ -79,51 +108,65 @@ class TestDatabaseConnection(unittest.TestCase):
         self.database.insert_congregation(**params)
 
     def test_congregation_bad_params(self):
-        bad_params = {"number":        -1,
-                      "energy_levels": 5,
-                      "period":        11,
-                      "metal":         True,
-                      "amphoteric":    True,
-                      "gas":           False,
-                      "semiconductor": False}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number":        12,
-                      "energy_levels": 0,
-                      "period":        11,
-                      "metal":         True,
-                      "amphoteric":    True,
-                      "gas":           False,
-                      "semiconductor": False}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number": 12,
-                      "energy_levels": 5,
-                      "period":        22,
-                      "metal":         True,
-                      "amphoteric":    True,
-                      "gas":           False,
-                      "semiconductor": False}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
-        bad_params = {"number":        12,
-                      "energy_levels": 5,
-                      "period":        11,
-                      "metal":         False,
-                      "amphoteric":    True,
-                      "gas":           False,
-                      "semiconductor": False}
-        self.assertRaises(AssertionError, self.database.insert_basic_params, **bad_params)
+        """Test if AssertionError raises on bad queries to congregation table"""
+        params = {"number":        -1,
+                  "energy_levels": 5,
+                  "metal":         True,
+                  "amphoteric":    True,
+                  "gas":           False,
+                  "semiconductor": False}
+        self.assertRaises(AssertionError,
+                          self.database.insert_congregation,
+                          **params)
+        params = {"number":        12,
+                  "energy_levels": 0,
+                  "metal":         True,
+                  "amphoteric":    True,
+                  "gas":           False,
+                  "semiconductor": False}
+        self.assertRaises(AssertionError,
+                          self.database.insert_congregation,
+                          **params)
+        self.assertRaises(AssertionError,
+                          self.database.insert_congregation,
+                          **params)
+        params = {"number":        12,
+                  "energy_levels": 5,
+                  "metal":         False,
+                  "amphoteric":    True,
+                  "gas":           False,
+                  "semiconductor": False}
+        self.assertRaises(AssertionError,
+                          self.database.insert_congregation,
+                          **params)
 
     def test_links(self):
-        self.database.insert_links(number=12, wiki_link="en.wikipedia.org/wiki/Magnesium")
+        """Test if right values can be inserted into links table"""
+        params = {"number":    12,
+                  "wiki_link": "en.wikipedia.org/wiki/Magnesium"}
+        self.database.insert_links(**params)
 
     def test_links_bad_params(self):
-        bad_params = (-3, "en.wikipedia.org/wiki/Magnesium")
-        self.assertRaises(AssertionError, self.database.insert_basic_params, bad_params)
-        bad_params = (12, "")
-        self.assertRaises(AssertionError, self.database.insert_basic_params, bad_params)
-        bad_params = (0, "en.wikipedia.org/wiki/Magnesium")
-        self.assertRaises(AssertionError, self.database.insert_basic_params, bad_params)
+        """Test if AssertionError raises on bad queries to links table"""
+        args = {"number":    -3,
+                "wiki_link": "en.wikipedia.org/wiki/Magnesium"}
+        self.assertRaises(AssertionError,
+                          self.database.insert_links,
+                          **args)
+        args = {"number":    12,
+                "wiki_link": ""}
+        self.assertRaises(AssertionError,
+                          self.database.insert_links,
+                          **args)
+        args = {"number":    0,
+                "wiki_link": "en.wikipedia.org/wiki/Magnesium"}
+        self.assertRaises(AssertionError,
+                          self.database.insert_links,
+                          **args)
 
     def tearDown(self):
-        self.database.__exit__()
-        with DatabaseCreateTable(self.config) as self.table_creator:
-            self.table_creator.drop_all_tables()
+        """Close database & drop all tables"""
+        self.database.close()
+        self.table_creator = DatabaseCreateTable(self.config)
+        self.table_creator.drop_all_tables()
+        self.table_creator.close()
