3e7ae41dce3e68ab496ef6621cf71f9ccd094edc
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-04 00:58:41 +0300

    refactoring
    convert tabs into 4 spaces
    add module and methods docstrings
    add with expression to i/o operations

--- src/config.py
+++ src/config.py
@@ -1,45 +1,48 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+"""Configuration class"""
 from os import path, mkdir, sep
 from tomli import load, TOMLDecodeError
-from src.errors_handler import ErrorHandler
+from src.errors_handler import show_exception
 
 
-class Configuration(object):
-	HOME = path.expanduser("~")
-	CONFIG_PATH = HOME + "/.IVPT/config.toml"
-	CONFIG = None
+class Configuration:
+    """Work with IVPT configuration file"""
+    home = path.expanduser("~")
+    config_path = home + "/.IVPT/config.toml"
+    config = None
+    default_config = "src/default_config.toml"
 
-	@classmethod
-	def touch_config(cls):
-		config_path = cls.CONFIG_PATH
-		path_items_list = list(config_path.split(sep))[1:]
-		for index in range(len(path_items_list)):
-			cur_dir = sep + sep.join(path_items_list[0: index])
-			if not path.isdir(cur_dir):
-				mkdir(cur_dir)
-		if not path.isfile(config_path):
-			default_config = Configuration.get_default_config("default_config.toml")
-			with open(config_path, "w") as input_file:
-				input_file.write(default_config)
+    @classmethod
+    def touch_config(cls):
+        """Make config file if it does not exist"""
+        path_items_list = list(cls.config_path.split(sep))[1:]
+        for index in range(len(path_items_list)):
+            cur_dir = sep + sep.join(path_items_list[0: index])
+            if not path.isdir(cur_dir):
+                mkdir(cur_dir)
+        if not path.isfile(cls.config_path):
+            contents = Configuration.get_default_config(cls.default_config)
+            with open(cls.config_path, "w", encoding="utf-8") as input_file:
+                input_file.write(contents)
 
-	@classmethod
-	def read_config(cls):
-		config_path = cls.CONFIG_PATH
-		try:
-			with open(config_path, "rb") as file:
-				toml_dict = load(file)
-				cls.CONFIG = toml_dict
-		except TOMLDecodeError as error:
-			ErrorHandler.show_exception(error)
-		except ValueError as error:
-			print("Invalid config file")
-			print("Probably, there is a problem with float values")
-			print("-" * 100)
-			ErrorHandler.show_exception(error)
+    @classmethod
+    def read_config(cls):
+        """Read config's contents"""
+        try:
+            with open(cls.config_path, "rb") as file:
+                toml_dict = load(file)
+                cls.config = toml_dict
+        except TOMLDecodeError as error:
+            show_exception(error)
+        except ValueError as error:
+            print("Invalid config file")
+            print("Probably, there is a problem with float values")
+            print("-" * 100)
+            show_exception(error)
 
-	@staticmethod
-	def get_default_config(config_path: str):
-		with open(config_path, "r") as output_file:
-			data = output_file.read()
-		return data
+    @staticmethod
+    def get_default_config(config_path: str):
+        """Get default config's contents"""
+        with open(config_path, "r", encoding="utf-8") as output_file:
+            data = output_file.read()
+        return data
