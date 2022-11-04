53d45e072d1ee0f2065a6bf10d91378e00e3bc10
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-04 18:14:53 +0300

    fix & refactor insert.py convert INSERT statements into INSERT OR REPLACE make docstrings more useful

--- src/database/insert.py
+++ src/database/insert.py
@@ -1,71 +1,83 @@
 #!/usr/bin/env python3
 """DatabaseInsert class"""
-from src.database.base import DatabaseConnection
-from src.chemistry import ChemistryLogicChecks
+from .chemistry import ChemistryLogicChecks as chemistry
+from .base import DatabaseConnection
 
 
 class DatabaseInsert(DatabaseConnection):
+
     """Insert data into database tables"""
-    def insert_basic_info(self, number: int, abb: str):
+
+    def insert_basic_info(self, number: int, abb: str) -> tuple:
         """
         Insert element's number and symbol into basic_info table
         """
-        number = ChemistryLogicChecks.check_number(number)
-        abb = ChemistryLogicChecks.make_abb(abb)
+        number = chemistry.check_number(number)
+        abb = chemistry.make_abb(abb)
         data = {"number": number, "abbreviation": abb}
         query = """
-        INSERT INTO basic_info
+        INSERT OR REPLACE INTO basic_info
         (number, abbreviation)
         VALUES (:number, :abbreviation);
         """
-        self.execute_query(query, data)
+        process = self.execute_query(query, data)
+        return process
 
-    def insert_basic_params(self, **kvargs):
+    def insert_basic_params(self, **kvargs) -> tuple:
         """
         Check & insert element's number, name, group, mass (weight) and period
         into basic_params table
         """
-        number = ChemistryLogicChecks.check_number(kvargs["number"])
-        name = ChemistryLogicChecks.make_name(kvargs["name"])
-        group = ChemistryLogicChecks.check_group(kvargs["grp"])
-        period = ChemistryLogicChecks.check_period(kvargs["period"])
-        weight = ChemistryLogicChecks.check_weight(kvargs["weight"])
-        period = ChemistryLogicChecks.check_period(kvargs["period"])
+        number = chemistry.check_number(kvargs["number"])
+        name = chemistry.make_name(kvargs["name"])
+        group = chemistry.check_group(kvargs["grp"])
+        period = chemistry.check_period(kvargs["period"])
+        weight = chemistry.check_weight(kvargs["weight"])
+        period = chemistry.check_period(kvargs["period"])
         data = {"number": number,
                 "name":   name,
                 "grp":    group,
                 "weight": weight,
                 "period": period}
         query = """
-        INSERT INTO basic_params
+        INSERT OR REPLACE INTO basic_params
         (number, name, grp, weight, period)
         VALUES (:number, :name, :grp, :weight, :period);
         """
-        self.execute_query(query, data)
+        process = self.execute_query(query, data)
+        return process
 
-    def insert_congregation(self,
-                            number:        int,
-                            energy_levels: int,
-                            metal=False,
-                            amphoteric=False,
-                            gas=False,
-                            semiconductor=False):
+    def insert_congregation(self, **kvargs) -> tuple:
         """
+        :number:        int,
+        :energy_levels: int,
+        :metal:         =False,
+        :amphoteric:    =False,
+        :gas:           =False,
+        :semiconductor: =False
         Check & insert element's number, energy_levels, period,
         metal-param, amphoteric-param, gas-param, semiconductor-param
         into congregation table
         """
-        number = ChemistryLogicChecks.check_number(number)
-        energy_levels = ChemistryLogicChecks.check_energy_levels(energy_levels)
-        ChemistryLogicChecks.check_amphoteric_metal(metal, amphoteric)
+        if "metal" not in kvargs:
+            kvargs["metal"] = False
+        if "amphoteric" not in kvargs:
+            kvargs["amphoteric"] = False
+        if "gas" not in kvargs:
+            kvargs["gas"] = False
+        if "semiconductor" not in kvargs:
+            kvargs["semiconductor"] = False
+        number = chemistry.check_number(kvargs["number"])
+        energy_levels = chemistry.check_energy_levels(kvargs["energy_levels"])
+        chemistry.check_amphoteric_metal(kvargs["metal"], kvargs["amphoteric"])
         data = {"number":        number,
                 "energy_levels": energy_levels,
-                "metal":         metal,
-                "amphoteric":    amphoteric,
-                "gas":           gas,
-                "semiconductor": semiconductor}
+                "metal":         kvargs["metal"],
+                "amphoteric":    kvargs["amphoteric"],
+                "gas":           kvargs["gas"],
+                "semiconductor": kvargs["semiconductor"]}
         query = """
-        INSERT INTO congregation 
+        INSERT OR REPLACE INTO congregation 
         (number, energy_levels, metal, amphoteric, gas, semiconductor)
         VALUES (:number,
                 :energy_levels,
@@ -74,16 +86,18 @@ class DatabaseInsert(DatabaseConnection):
                 :gas, 
                 :semiconductor);
         """
-        self.execute_query(query, data)
+        process = self.execute_query(query, data)
+        return process
 
-    def insert_links(self, number: int, wiki_link: str):
+    def insert_links(self, number: int, wiki_link: str) -> tuple:
         """Check & insert elements's name and wiki link into links table"""
-        number = ChemistryLogicChecks.check_number(number)
-        wiki_link = ChemistryLogicChecks.check_link(wiki_link)
+        number = chemistry.check_number(number)
+        wiki_link = chemistry.check_link(wiki_link)
         data = {"number": number, "wiki_link": wiki_link}
         query = """
-        INSERT INTO links
+        INSERT OR REPLACE INTO links
         (number, wiki_link)
         VALUES (:number, :wiki_link);
         """
-        self.execute_query(query, data)
+        process = self.execute_query(query, data)
+        return process
