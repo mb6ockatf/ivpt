20554d4b625ea5ec7e33f31143c44e67d495c68d
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-05 22:24:52 +0300

    refactoring: convert tabs into spaces

--- src/interfaces/hyper_link_label.py
+++ src/interfaces/hyper_link_label.py
@@ -4,9 +4,9 @@ from PyQt5.QtWidgets import QLabel
 
 
 class HyperlinkLabel(QLabel):
-	"""Create clickable links"""
-	def __init__(self, parent=None):
-		super().__init__()
-		self.setStyleSheet("font-size: 20px")
-		self.setOpenExternalLinks(True)
-		self.setParent(parent)
+    """Create clickable links"""
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setStyleSheet("font-size: 20px")
+        self.setOpenExternalLinks(True)
+        self.setParent(parent)
