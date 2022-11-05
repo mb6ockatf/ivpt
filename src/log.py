15923154aecc61610638ab5a5cc21d826de02248
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-05 12:42:39 +0300

    fix: add log file existence check

--- src/log.py
+++ src/log.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-
+from os import path, mkdir, sep
 from logging import basicConfig, DEBUG
 
 
 def setup_logging(log_name: str):
-    basicConfig(filename=log_name, level=DEBUG, format="%(asctime)s %(message)s")
+	home = path.expanduser("~")
+	log_path = home + log_name
+	path_items_list = list(log_path.split(sep))[1:]
+	for index in range(len(path_items_list)):
+		cur_dir = sep + sep.join(path_items_list[0: index])
+		if not path.isdir(cur_dir):
+			mkdir(cur_dir)
+	if not path.isfile(log_path):
+		with open(log_path, "w", encoding="utf-8"):
+			...
+	basicConfig(filename=log_path, level=DEBUG, format="%(asctime)s %(message)s")
