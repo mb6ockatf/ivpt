de29609f60ab1d5c323d5dde8bdedcb4b98defa9
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-05 22:26:06 +0300

    fix: docstrings

--- src/essential/errors_handler.py
+++ src/essential/errors_handler.py
@@ -6,11 +6,11 @@ from logging import error
 
 
 def show_exception(exception_object):
-	"""Print pretty exception"""
-	exc_type, exc_value, exc_tb = exc_info()
-	pretty_exception = format_exception(exc_type, exc_value, exc_tb)
-	error(pretty_exception)
-	error_string = " ".join(list(map(str, exception_object.args)))
-	print("Error:", error_string)
-	print("Exception class:", exception_object.__class__)
-	print("Traceback:", "\n", "\n".join(pretty_exception))
+    """Print pretty traceback"""
+    exc_type, exc_value, exc_tb = exc_info()
+    pretty_exception = format_exception(exc_type, exc_value, exc_tb)
+    error(pretty_exception)
+    error_string = " ".join(list(map(str, exception_object.args)))
+    print("Error:", error_string)
+    print("Exception class:", exception_object.__class__)
+    print("Traceback:", "\n", "\n".join(pretty_exception))
