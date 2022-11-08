aa1b0e215e38cf5e009ac67331ea49d81c703232
mb6ockatf <mb6ockatf@relay.firefox.com>
2022-11-09 00:02:38 +0300

    fix: buttons not showing

--- src/interfaces/stoichiometry.py
+++ src/interfaces/stoichiometry.py
@@ -16,30 +16,30 @@ class StoichiometryPage(QMainWindow):
         self.slots = 10
         self.setGeometry(300, 300, width, height)
         self.setWindowTitle("Stoichiometry")
+        self.setFixedWidth(width)
+        self.setFixedHeight(height)
         self.rows_count = 0
         self.things = []
         self.add_row_btn = QPushButton("+", self)
         self.add_row_btn.resize(350, 30)
-        self.add_row_btn.move(30, 30 + 120)
+        self.add_row_btn.move(30, 30)
         self.add_row_btn.clicked.connect(self.draw_line)
-        self.draw_line()
 
     def draw_line(self):
-        """Новые input_frame и eval_button не создаются"""
         self.input_frame = QTextEdit(self)
         self.input_frame.resize(350, 100)
         self.input_frame.move(30, 30 + 120 * self.rows_count)
         placeholder = """Enter formula
 For instance, H2SO4"""
         self.input_frame.setPlaceholderText(placeholder)
+        self.input_frame.show()
         self.eval_button = QPushButton("eval", self)
         self.eval_button.resize(100, 100)
         self.eval_button.move(400, 30 + 120 * (self.rows_count))
+        self.eval_button.show()
         self.add_row_btn.move(30, 30 + 120 * (self.rows_count + 1))
         self.things += [self.input_frame]
         self.things += [self.eval_button]
-        print(self.things)
         self.rows_count += 1
-        print(self.rows_count)
-        if self.rows_count == 10:
+        if self.rows_count == 6:
             self.add_row_btn.hide()
